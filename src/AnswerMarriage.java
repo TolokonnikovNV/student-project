a6db4ee47a0bf68933fdbaa0e71713a26ea03692
Nikita Tolokonnikov <tolokonnikov.nv@gmail.com>
2023-07-24 21:10:47 +0700

    add loops and conditions for checkCityRegister

--- src/AnswerMarriage.java
+++ src/AnswerMarriage.java
@@ -1,2 +1,3 @@
 public class AnswerMarriage {
+    boolean success;
 }
--- src/StudentOrderValidator.java
+++ src/StudentOrderValidator.java
@@ -3,46 +3,58 @@ public class StudentOrderValidator {
         checkAll();
     }
 
-    static void checkAll(){
-        StudentOrder so = readStudentOrder();
+    static void checkAll() {
+        while (true) {
+            StudentOrder so = readStudentOrder();
+            if (so == null) {
+                break;
+            }
+            AnswerCityRegister answerCityRegister = checkCityRegister(so);
+            if (!answerCityRegister.success){
+                continue;
+            }
+            AnswerMarriage answerMarriage = checkMarriage(so);
+            AnswerChildren answerChildren = checkChildren(so);
+            AnswerStudent answerStudent = checkStudent(so);
 
-        AnswerCityRegister answerCityRegister = checkCityRegister(so);
-        AnswerMarriage answerMarriage = checkMarriage(so);
-        AnswerChildren answerChildren = checkChildren(so);
-        AnswerStudent answerStudent = checkStudent(so);
+            sendMail(so);
 
-        sendMail(so);
+            so = readStudentOrder();
+        }
     }
 
-    static AnswerCityRegister checkCityRegister(StudentOrder so){
+
+    static AnswerCityRegister checkCityRegister(StudentOrder so) {
         System.out.println("checkCityRegister");
         AnswerCityRegister answerCityRegister = new AnswerCityRegister();
+        answerCityRegister.success = false;
         return answerCityRegister;
     }
 
-    static AnswerMarriage checkMarriage(StudentOrder so){
+    static AnswerMarriage checkMarriage(StudentOrder so) {
         System.out.println("checkMarriage");
         AnswerMarriage answerMarriage = new AnswerMarriage();
         return answerMarriage;
     }
 
-    static AnswerChildren checkChildren(StudentOrder so){
+    static AnswerChildren checkChildren(StudentOrder so) {
         System.out.println("checkChildren");
         AnswerChildren answerChildren = new AnswerChildren();
         return answerChildren;
     }
 
-    static AnswerStudent checkStudent(StudentOrder so){
+    static AnswerStudent checkStudent(StudentOrder so) {
         System.out.println("checkStudent");
         AnswerStudent answerStudent = new AnswerStudent();
         return answerStudent;
     }
 
-    static StudentOrder readStudentOrder(){
-        return new StudentOrder();
+    static StudentOrder readStudentOrder() {
+        StudentOrder studentOrder = new StudentOrder();
+        return studentOrder;
     }
 
-    static void sendMail(StudentOrder so){
-
+    static void sendMail(StudentOrder so) {
+        System.out.println("Sending mail");
     }
 }
