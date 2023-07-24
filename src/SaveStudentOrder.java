bcb54e209c87b556ac6e780c906eab47700ffb3c
Nikita Tolokonnikov <tolokonnikov.nv@gmail.com>
2023-07-24 20:05:15 +0700

    add StudentOrder

--- src/SaveStudentOrder.java
+++ src/SaveStudentOrder.java
@@ -1,9 +1,20 @@
 public class SaveStudentOrder {
     public static void main(String[] args) {
-        saveStudentOrder();
+        StudentOrder so = new StudentOrder();
+        so.hFirstName = "Alex";
+        so.hLastName = "Petrov";
+        so.wFirstName = "Galina";
+        so.wLastName = "Petrova";
+
+        long ans = saveStudentOrder(so);
+        System.out.println(ans);
     }
 
-    static void saveStudentOrder(){
-        System.out.println("saveStudentOrder is running");
+    static long saveStudentOrder(StudentOrder studentOrder){
+        long answer;
+        answer = 199;
+        System.out.println("saveStudentOrder: " + studentOrder.hFirstName + " " + studentOrder.hLastName + " " +
+                                                  studentOrder.wFirstName + " " + studentOrder.wLastName);
+        return answer;
     }
 }
