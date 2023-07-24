5e299c6f56f997e40d314f3693437ab9ce2b5da5
Nikita Tolokonnikov <tolokonnikov.nv@gmail.com>
2023-07-24 19:45:24 +0700

    add StudentOrderValidator

---
+++ src/StudentOrderValidator.java
@@ -0,0 +1,28 @@
+public class StudentOrderValidator {
+    public static void main(String[] args) {
+        checkAll();
+    }
+
+    static void checkAll(){
+        checkCityRegister();
+        checkMarriage();
+        checkChildren();
+        checkStudent();
+    }
+
+    static void checkCityRegister(){
+        System.out.println("checkCityRegister");
+    }
+
+    static void checkMarriage(){
+        System.out.println("checkMarriage");
+    }
+
+    static void checkChildren(){
+        System.out.println("checkChildren");
+    }
+
+    static void checkStudent(){
+        System.out.println("checkStudent");
+    }
+}
