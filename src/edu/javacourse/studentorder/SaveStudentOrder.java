56a4c65fd2fc906f64b1bbe8892ca8f6ff4710f5
Nikita Tolokonnikov <tolokonnikov.nv@gmail.com>
2023-07-25 17:29:06 +0700

    add student parameters

--- src/edu/javacourse/studentorder/SaveStudentOrder.java
+++ src/edu/javacourse/studentorder/SaveStudentOrder.java
@@ -1,14 +1,11 @@
 package edu.javacourse.studentorder;
 
+import edu.javacourse.studentorder.domain.Adult;
 import edu.javacourse.studentorder.domain.StudentOrder;
 
 public class SaveStudentOrder {
     public static void main(String[] args) {
         StudentOrder so = new StudentOrder();
-        so.sethFirstName("Alex");
-        so.sethLastName("Petrov");
-        so.setwFirstName("Galina");
-        so.setwFirstName("Petrova");
 
         long ans = saveStudentOrder(so);
         System.out.println(ans);
@@ -16,8 +13,15 @@ public class SaveStudentOrder {
 
     static long saveStudentOrder(StudentOrder studentOrder){
         long answer = 199;
-        System.out.println("saveStudentOrder: " + studentOrder.gethFirstName() + " " + studentOrder.gethLastName()
-                + " " +                           studentOrder.getwFirstName() + " " + studentOrder.getwLastName());
+        System.out.println("saveStudentOrder: ");
         return answer;
+    }
+
+    static StudentOrder buildStudentOrder(){
+        StudentOrder studentOrder = new StudentOrder();
+        Adult husband = new Adult();
+        husband.setGivenName("Andrey");
+        studentOrder.setHusband(husband);
+        return studentOrder;
     }
 }
